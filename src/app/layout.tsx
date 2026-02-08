20b1c0f9f772e6635ae2e193ea914db00f8c3f12
Gabriel William de Paulo <gabrielwilliam208@gmail.com>
2026-02-08 11:15:11 -0300

    feat(analytics): add Microsoft Clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -139,6 +139,15 @@ gtag('config', 'G-HD00424MR7');`}
         <MaterialSymbolsLoader />
         <Header />
         {children}
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "ve74erf449");
+  `}
+        </Script>
         <FloatingWhatsApp />
       </body>
     </html>
